5143768697143547019f6574893383a1a8ef0d42
appolinair2355 <felicitationsbien990@gmail.com>
2026-03-07 07:50:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ Configuration du bot Telegram de prédiction Pair/Impair
 
 API_ID = 29177661
 API_HASH = 'a8639172fa8d35dbfd8ea46286d349ab'
-BOT_TOKEN = '7749786995:AAGr9rk_uuykLLp5g7Hi3XwIlsdMfW9pWFw'
+BOT_TOKEN = '8442253971:AAEisYucgZ49Ej2b-mK9_6DhNrqh9WOc_XU'
 ADMIN_ID = 1190237801
 
 # ============================================
